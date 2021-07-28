22034b5cf9e212216c8e0187d4767143728d83a2
Yuvaraj <15032282+yuvgeek@users.noreply.github.com>
2021-07-28 22:31:51 +0530

    minor updates to header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   appName = environment.appName;
 
   sideNavItems = [
@@ -15,4 +17,14 @@ export class AppComponent {
     { label: 'Feed', route: 'feed', icon: 'feed' },
     { label: 'Categories', route: '/categories', icon: 'category' },
   ];
+
+  isLoading: boolean = true;
+
+  constructor(public userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.userInfoObs$.pipe().subscribe(() => {
+      this.isLoading = false;
+    });
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,50 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { UserService } from '../services/user.service';
-import type { UserResource } from '@clerk/types';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, filter } from 'rxjs/operators';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
   @Input() appName: string = '';
+  @ViewChild('userAction', { static: false }) private userActionContainer:
+    | ElementRef<HTMLDivElement>
+    | undefined;
+
   constructor(
     private router: Router,
     public userService: UserService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private activatedRoute: ActivatedRoute
   ) {}
 
-  ngOnInit(): void {
-    console.log(
-      this.userService.userInfoObs$.subscribe((val) => console.log(val))
-    );
+  ngAfterViewInit(): void {
+    this.userService.userInfoObs$
+      .pipe(
+        filter((res) => !!res),
+        delay(2000)
+      )
+      .subscribe((res) => {
+        console.log(res);
+
+        const el = this.userActionContainer?.nativeElement as HTMLDivElement;
+
+        setTimeout(() => {
+          window.Clerk.mountUserButton(el);
+        }, 2000);
+      });
   }
+
   navigateToHome() {
     this.router.navigate(['']);
   }
